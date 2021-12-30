fbd73efe8990d1b4e5e20b73ec6fe458d689ebd9
cgaaf <cgaafary@gmail.com>
2021-12-30 10:22:16 -0500

    Changed UserType protocol Built PreviewContainer - Not functional at this point

--- Sources/SwiftUIFirebaseAuth/UserTypeProtocol.swift
+++ Sources/SwiftUIFirebaseAuth/UserTypeProtocol.swift
@@ -8,30 +8,17 @@
 import Foundation
 import Firebase
 
-public protocol UserType: Codable, Identifiable {
-    var id: String { get }
-    var email: String { get }
-    var displayName: String { get }
-    
-    init(from firebaseUser: User)
-    init(id: String, email: String, displayName: String)
-}
-
-public extension UserType {
-    init(from firebaseUser: User) throws {
-        guard let firebaseEmail = firebaseUser.email else {
-            throw UserTypeInitError.noFirebaseEmail
-        }
-        
-        guard let firebaseDisplayName = firebaseUser.displayName else {
-            throw UserTypeInitError.noFirebaseDisplayName
-        }
-        
-        self.init(id: firebaseUser.uid, email: firebaseEmail, displayName: firebaseDisplayName)
-    }
+public protocol UserType {
+    var uid: String { get }
+    var email: String? { get }
+    var displayName: String? { get }
 }
 
 public enum UserTypeInitError: Error {
     case noFirebaseEmail
     case noFirebaseDisplayName
 }
+
+extension User: UserType {}
+
+
